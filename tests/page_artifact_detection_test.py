f86db70b5afb74e96fdd3ec789bfa0f37b667678
Alex Ostreiko <esaul314@gmail.com>
2025-08-12 14:30:07 -0400

    fix: simplify footnote marker normalization

--- tests/page_artifact_detection_test.py
+++ tests/page_artifact_detection_test.py
@@ -75,11 +75,6 @@ class TestPageArtifactDetection(unittest.TestCase):
         cleaned = remove_page_artifact_lines(text, 2)
         self.assertEqual(cleaned, "Can exist[3]. This is next")
 
-    def test_superscript_footnote_marker(self):
-        text = "Can exist.\u00b3\n\u00b3 Footnote text.\n\nThis is next"
-        cleaned = remove_page_artifact_lines(text, 2)
-        self.assertEqual(cleaned, "Can exist[3]. This is next")
-
     def test_crlf_footnote_marker(self):
         text = "Can exist.3\r\n3 Footnote text.\r\n\r\nThis is next"
         cleaned = remove_page_artifact_lines(text, 2)
